2b014c7b5775fd9cfd65f2749265312fe201a867
sohelst9 <you@example.websohel89@gmail.com>
2023-10-09 01:06:48 +0600

    add-content-section

--- resources/lang/en/data.php
+++ resources/lang/en/data.php
@@ -4,6 +4,8 @@ return [
     'site_title' => 'Lara Multiple Language demo',
     'header_title' => 'Our Blogs',
     'post_button' => 'Read More.',
+    'english' => 'English',
+    'bangla' => 'Bangla',
 
     //----first post---
     'first_post_title' => 'What is Web Designing ?',
@@ -15,4 +17,7 @@ return [
     'second_post_title' => 'What Do Web Designers Do ?',
     'second_post_description' => 'Web designers create visually appealing and functional websites, designing layouts, graphics, and user interfaces to enhance online user experiences.',
     'second_post_author' => 'Author : Rasel Ahmed',
+
+    //----content section
+    'content_desc' => 'While Simple Programmer was originally created by founder John Sonmez to distill what he learned as a web developer into understandable content, today it is focused on helping software developers, programmers and other IT professionals improve their careers and their lives.  This blog gets into the nitty-gritty of being a web developer and if you’ve wondered about something web development related, there’s a good chance they’ve written about it. Simple Programmer puts a fun spin on videos, keeping you engaged with humor while providing valuable content. They’re constantly posting new articles – almost daily – so you’ll never be bored.'
 ];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,41 +11,45 @@
 
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col-md-10 m-auto">
-                <div class="card mt-5">
-                    <div class="card-header bg-dark d-flex align-item-center justify-content-between">
-                        <h3 class="text-light">{{ __('data.header_title') }}</h3>
-                        <div class="select_language">
-                            <select name="language" id="" class="form-control changeLang">
-                                <option value="en" {{ session()->get('locate') == 'en' ? 'selected': '' }}>English</option>
-                                <option value="bn" {{ session()->get('locate') == 'bn' ? 'selected' : '' }}>Bangla</option>
-                            </select>
+        <section id="blog">
+            <div class="row">
+                <div class="col-md-10 m-auto">
+                    <div class="card mt-5">
+                        <div class="card-header bg-dark d-flex align-item-center justify-content-between">
+                            <h3 class="text-light">{{ __('data.header_title') }}</h3>
+                            <div class="select_language">
+                                <select name="language" id="" class="form-control changeLang">
+                                    <option value="en" {{ session()->get('locate') == 'en' ? 'selected' : '' }}>
+                                        {{ __('data.english') }}</option>
+                                    <option value="bn" {{ session()->get('locate') == 'bn' ? 'selected' : '' }}>
+                                        {{ __('data.bangla') }}</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="blog">
-                                    <h4 class="title">{{ __('data.first_post_title') }}</h4>
-                                    <p class="description">
-                                       {{ __('data.first_post_description') }}
-                                    </p>
-                                    <div class="blog_footer">
-                                        <h6>{{ __('data.first_post_author') }}</h6>
-                                        <a href="" class="read_more">{{ __('data.post_button') }}</a>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="blog">
+                                        <h4 class="title">{{ __('data.first_post_title') }}</h4>
+                                        <p class="description">
+                                            {{ __('data.first_post_description') }}
+                                        </p>
+                                        <div class="blog_footer">
+                                            <h6>{{ __('data.first_post_author') }}</h6>
+                                            <a href="" class="read_more">{{ __('data.post_button') }}</a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="blog">
-                                    <h4 class="title">{{ __('data.second_post_title') }}</h4>
-                                    <p class="description">
-                                        {{ __('data.second_post_description') }}
-                                    </p>
-                                    <div class="blog_footer">
-                                        <h6>{{ __('data.second_post_author') }}</h6>
-                                        <a href="" class="read_more">{{ __('data.post_button') }}</a>
+                                <div class="col-md-6">
+                                    <div class="blog">
+                                        <h4 class="title">{{ __('data.second_post_title') }}</h4>
+                                        <p class="description">
+                                            {{ __('data.second_post_description') }}
+                                        </p>
+                                        <div class="blog_footer">
+                                            <h6>{{ __('data.second_post_author') }}</h6>
+                                            <a href="" class="read_more">{{ __('data.post_button') }}</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -53,14 +57,24 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+
+        <section id="content">
+            <div class="row">
+                <div class="col-md-10 m-auto">
+                    <p>
+                        {{ __('data.content_desc') }}
+                    </p>
+                </div>
+            </div>
+        </section>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <script>
         var url = "{{ route('change_language') }}";
-        $('.changeLang').change(function(){
+        $('.changeLang').change(function() {
             window.location.href = url + '?language=' + $(this).val();
         })
     </script>
